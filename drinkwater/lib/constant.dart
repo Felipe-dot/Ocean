2693aed4c9a1db3c8cc11663a5f62cf9fae2f446
Luis Felipe de Azevedo Melo <felipeluis610@gmail.com>
2022-01-19 18:25:12 -0300

    Adicionei os assets e cores do aplicativo

---
+++ drinkwater/lib/constant.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// Paleta de cores
+const Color kMainColor = Color(0xFF4C7AC8);
+const Color kDark1 = Color(0xFF3E5189);
+const Color kDark2 = Color(0xFF25325C);
+const Color kLightBlue1 = Color(0xFF8DA6DE);
+const Color kLightBlue2 = Color(0xFFB2C7E6);
+const Color kLightBlue3 = Color(0xFFD8E5F8);
+const Color kLightBlue4 = Color(0xFFF5F9FF);
+const Color kGreenAccent = Color(0xFF8CF57B);
+const Color kRedAccent = Color(0xFFC25050);
+const Color kGray = Color(0xFF727272);
+const Color kBlack = Color(0xFF2F2E41);
+const Color kWhite = Color(0xFFFFFFFF);
