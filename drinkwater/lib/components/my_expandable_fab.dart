40fcf2fff418969308a84ecd5917bdff8b86d2c7
Luis Felipe de Azevedo Melo <felipeluis610@gmail.com>
2022-02-20 15:39:33 -0300

    Adicionando responsividade ao fab

--- drinkwater/lib/components/my_expandable_fab.dart
+++ drinkwater/lib/components/my_expandable_fab.dart
@@ -174,7 +174,8 @@ class _ExpandingActionButton extends StatelessWidget {
           progress.value * maxDistance,
         );
         return Positioned(
-          right: 157.0 + offset.dx,
+          right: MediaQuery.of(context).size.height / 4.15 + offset.dx,
+          // right: 157.0 + offset.dx,
           bottom: 4.0 + offset.dy,
           child: Transform.rotate(
             angle: (1.0 - progress.value) * math.pi / 2,
