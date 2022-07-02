44774fd1c8bfb352bc5f0b672ed39d75014d8483
Luis Felipe de Azevedo Melo <felipeluis610@gmail.com>
2022-07-02 16:19:22 -0300

    OA-18 Refactoring Weekend Streak

---
+++ drinkwater/lib/components/my_mock_streak.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../constant.dart';
+
+class MyMockStreak extends StatelessWidget {
+  const MyMockStreak({Key key, this.weekday}) : super(key: key);
+
+  final String weekday;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          backgroundColor: kMainColor.withOpacity(0.1),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Text(
+          weekday,
+          style: const TextStyle(
+            fontSize: 13,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ],
+    );
+  }
+}
