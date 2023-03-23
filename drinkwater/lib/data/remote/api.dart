a49f452436c9bdabed0e9785abd1ced2939291a6
Felipe-dot <felipeluis610@gmail.com>
2023-03-23 10:29:45 -0300

    adding create user data function

--- drinkwater/lib/data/remote/api.dart
+++ drinkwater/lib/data/remote/api.dart
@@ -1,19 +1,4 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
-
-/* 
-        TODOs 
-    1 - Cadastrar um usuário - OK
-    2 - Realizar login do usuário - OK
-    3 - Inserir os dados de um usuário - OK 
-    4 - Modificar o email do usuário
-    5 - Modificar a meta de consumo de água
-    6 - Modificar a hora de acordar
-    7 - Modificar a hora de dormir
-    8 - Modificar o peso do usuário
-    9 - Deletar os dados de um usuário
-*/
 
 class Api {
   Future<String> signUp(email, password) async {
@@ -77,149 +62,56 @@ class Api {
       return response;
     }
   }
-}
 
-
-/* class ControllersDataSourceRemoteImpl implements ApiRemoteDataSource {
-  @override
-  Future<List<BookDto>> getAllBooks() async {
+  Future<dynamic> createUserData(
+      String? userToken,
+      int waterIntakeGoal,
+      DateTime wakeUpTime,
+      DateTime sleepTime,
+      int userWeight,
+      List<DateTime> notificationTimeList) async {
     Response response;
     Dio dio = Dio();
-    dio.options.headers['X-Parse-Application-Id'] =
-        'mX7qL9mfiKCPhEgnWkmBXU6dcM9nkTkOXbvBpqV3';
-    dio.options.headers['X-Parse-REST-API-Key'] =
-        'EDNEjUQlltCDcDB7kVxKjn2lkHG21hijexHbBlbk';
-    dio.options.headers['Content-Type'] = ' application/json';
 
-    response = await dio.post(
-        "https://parseapi.back4app.com/parse/functions/get-list-book",
-        data: '');
-    if (response.statusCode == 200) {
-      return BooksDto.fromJson(response.data).book as List<BookDto>;
-    } else {
-      return [];
+    dio.options.headers['X-Parse-Application-Id'] =
+        'iO2pQe0E4ZPEvmz3KSqWKPApMw7kAm5t1JZdwZGe';
+    dio.options.headers['X-Parse-REST-API-Key'] =
+        'IIJa91tLKGr1tMpbUzJCpoB7Nxtwfoh4SUbDhRGa';
+    dio.options.headers['X-Parse-Session-Token'] = userToken;
+
+    List<String> test = [];
+
+    notificationTimeList.forEach((v) => test.add(v.toIso8601String()));
+
+    print("---------------------");
+    print("--- Ola userToken $userToken");
+    print("--- Ola waterIntakeGoal $waterIntakeGoal");
+    print("--- Ola wakeUpTime $wakeUpTime");
+    print("--- Ola sleepTime $sleepTime");
+    print("--- Ola userWeight $userWeight");
+    print("--- Ola notification $test");
+    print("---------------------");
+
+    try {
+      response = await dio.post(
+          "https://parseapi.back4app.com/parse/functions/create-userData",
+          data: {
+            "waterIntakeGoal": waterIntakeGoal,
+            "wakeUpTime": wakeUpTime.toIso8601String(),
+            "sleepTime": sleepTime.toIso8601String(),
+            "userWeight": userWeight,
+            "notificationTimeList": test
+          });
+      print("MINHA RESPONSE ${response}");
+    } catch (e) {
+      print("meu erro e $e");
+      return e;
     }
-  }
-
-  @override
-  Future<bool> removeBook(bookId) async {
-    Response response;
-    Dio dio = Dio();
-    dio.options.headers['X-Parse-Application-Id'] =
-        'mX7qL9mfiKCPhEgnWkmBXU6dcM9nkTkOXbvBpqV3';
-    dio.options.headers['X-Parse-REST-API-Key'] =
-        'EDNEjUQlltCDcDB7kVxKjn2lkHG21hijexHbBlbk';
-    dio.options.headers['Content-Type'] = ' application/json';
-
-    response = await dio.post(
-        'https://parseapi.back4app.com/parse/functions/delete-book',
-        data: {"bookId": bookId});
-    if (response.statusCode == 200) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  @override
-  Future<String> createBook(BookEntity book) async {
-    print(book.amount.runtimeType);
-    print(book.amount);
-
-    Response response;
-    Dio dio = Dio();
-    dio.options.headers['X-Parse-Application-Id'] =
-        'mX7qL9mfiKCPhEgnWkmBXU6dcM9nkTkOXbvBpqV3';
-    dio.options.headers['X-Parse-REST-API-Key'] =
-        'EDNEjUQlltCDcDB7kVxKjn2lkHG21hijexHbBlbk';
-    dio.options.headers['Content-Type'] = ' application/json';
-
-    response = await dio.post(
-      'https://parseapi.back4app.com/parse/functions/create-book',
-      data: {
-        "title": book.title.toString(),
-        "description": book.description.toString(),
-        "author": book.author.toString(),
-        "amount": 1,
-      },
-      options: Options(
-        followRedirects: false,
-        // will not throw errors
-        validateStatus: (status) => true,
-        // headers: headers,
-      ),
-    );
 
     if (response.statusCode == 200) {
-      log('Livro criado com sucesso');
-      return 'Livro criado com sucesso';
+      return response.data;
     } else {
-      log('Erro ao criar livro');
-      return 'Erro ao criar livro';
-    }
-  }
-
-  @override
-  Future<bool> updateBook(BookDto book) async {
-    Response responseBookUpdateTitle;
-    Response responseBookUpdateAuthor;
-    Response responseBookUpdateDescription;
-    Response responseBookUpdateAmount;
-
-    Dio dio = Dio();
-    dio.options.headers['X-Parse-Application-Id'] =
-        'mX7qL9mfiKCPhEgnWkmBXU6dcM9nkTkOXbvBpqV3';
-    dio.options.headers['X-Parse-REST-API-Key'] =
-        'EDNEjUQlltCDcDB7kVxKjn2lkHG21hijexHbBlbk';
-    dio.options.headers['Content-Type'] = ' application/json';
-
-    responseBookUpdateTitle = await dio.post(
-        "https://parseapi.back4app.com/parse/functions/edit-book-title",
-        data: {
-          "bookId": book.objectId,
-          "title": book.title,
-        });
-
-    responseBookUpdateAuthor = await dio.post(
-        "https://parseapi.back4app.com/parse/functions/edit-book-author",
-        data: {"bookId": book.objectId, "author": book.author});
-
-    responseBookUpdateDescription = await dio.post(
-        "https://parseapi.back4app.com/parse/functions/edit-book-description",
-        data: {"bookId": book.objectId, "description": book.description});
-
-    responseBookUpdateAmount = await dio.post(
-        "https://parseapi.back4app.com/parse/functions/edit-book-amount",
-        data: {"bookId": book.objectId, "amount": book.amount});
-
-    if (responseBookUpdateTitle.statusCode == 200 &&
-        responseBookUpdateAuthor.statusCode == 200 &&
-        responseBookUpdateDescription.statusCode == 200 &&
-        responseBookUpdateAmount.statusCode == 200) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  @override
-  Future<BookEntity?> getBookDetails(String bookId) async {
-    Response response;
-    Dio dio = Dio();
-    dio.options.headers['X-Parse-Application-Id'] =
-        'mX7qL9mfiKCPhEgnWkmBXU6dcM9nkTkOXbvBpqV3';
-    dio.options.headers['X-Parse-REST-API-Key'] =
-        'EDNEjUQlltCDcDB7kVxKjn2lkHG21hijexHbBlbk';
-    dio.options.headers['Content-Type'] = ' application/json';
-
-    response = await dio.post(
-        'https://parseapi.back4app.com/parse/functions/get-book',
-        data: {"bookId": bookId});
-    if (response.statusCode == 200) {
-      return BookDto.fromJson(response.data['result']);
-    } else {
-      return null;
+      return response;
     }
   }
 }
- */
