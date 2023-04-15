fa47a028668c2dc329b7202433d2381ac61a3670
Felipe-dot <felipeluis610@gmail.com>
2023-04-15 16:13:24 -0300

    added create UserData and WaterHistory function

--- drinkwater/lib/utils/my_utils.dart
+++ drinkwater/lib/utils/my_utils.dart
@@ -1,3 +1,7 @@
+import 'package:drinkwater/models/waterHistory.dart';
+
+import '../models/userData.dart';
+
 List<DateTime> createNotificationTimeList(
   int wakeTimeHour,
   int wakeTimeMinute,
@@ -27,8 +31,7 @@ List<DateTime> createNotificationTimeList(
   var awakeTime = -1 * (wakeUpTime.difference(sleepTime).inHours);
 
   for (int x = 0; x < awakeTime; x++) {
-    var timeModified =
-        notificationTimeList[x].add(const Duration(hours: 1, minutes: 30));
+    var timeModified = notificationTimeList[x].add(const Duration(minutes: 50));
     if (sleepTime.compareTo(timeModified) != 1) {
       break;
     }
@@ -37,4 +40,41 @@ List<DateTime> createNotificationTimeList(
   notificationTimeList.add(sleepTime);
 
   return notificationTimeList;
+}
+
+UserData createUserData(var user) {
+  var notificationTimeListString = user['notificationTimeList'];
+  List<DateTime> notificationTimeList = [];
+  notificationTimeListString.forEach((e) {
+    notificationTimeList.add(DateTime.parse(e));
+  });
+
+  return UserData(
+    id: user['id'],
+    waterIntakeGoal: user['waterIntakeGoal'],
+    userWeight: user['userWeight'],
+    wakeUpTime: DateTime.parse(user['wakeUpTime']),
+    sleepTime: DateTime.parse(user['sleepTime']),
+    notificationTimeList: notificationTimeList,
+  );
+}
+
+List<WaterHistory> createAListOfWaterHistory(List<dynamic> waterHistory) {
+  List<WaterHistory> waterHistoryList = [];
+  waterHistory.forEach((element) {
+    waterHistoryList.add(WaterHistory(
+      id: element['id'],
+      statusDay: DateTime.parse(element['statusDay']),
+      goalOfTheDayWasBeat: element['goalOfTheDayWasBeat'],
+      amountOfWaterDrank: element['amountOfWaterDrank'],
+      drinkFrequency: element['drinkFrequency'],
+    ));
+  });
+
+  return waterHistoryList;
+}
+
+int howMuchINeedToDrink(int myWeight) {
+  // Calculando quanto o usuário deve beber
+  return myWeight * 35;
 }
