b1a4dc0c993b793692957940a6ab931bd8710ccb
Felipe-dot <felipeluis610@gmail.com>
2023-05-10 11:26:44 -0300

    using awesome notification

---
+++ drinkwater/lib/services/notification_service.dart
@@ -0,0 +1,132 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:drinkwater/screens/screens.dart';
+import 'package:flutter/material.dart';
+
+import '../constant.dart';
+import '../main.dart';
+
+class NotificationService {
+  static Future<void> initializeNotification() async {
+    await AwesomeNotifications().initialize(
+      null,
+      [
+        NotificationChannel(
+          channelGroupKey: 'high_importance_channel',
+          channelKey: 'high_importance_channel',
+          channelName: 'Basic notifications',
+          channelDescription: 'Notification channel for basic tests',
+          defaultColor: const Color(0xFF9D50DD),
+          ledColor: Colors.white,
+          importance: NotificationImportance.Max,
+          channelShowBadge: true,
+          onlyAlertOnce: true,
+          playSound: true,
+          criticalAlerts: true,
+        )
+      ],
+      channelGroups: [
+        NotificationChannelGroup(
+          channelGroupKey: 'high_importance_channel_group',
+          channelGroupName: 'Group 1',
+        )
+      ],
+      debug: true,
+    );
+
+    await AwesomeNotifications().isNotificationAllowed().then(
+      (isAllowed) async {
+        if (!isAllowed) {
+          await AwesomeNotifications().requestPermissionToSendNotifications();
+        }
+      },
+    );
+
+    await AwesomeNotifications().setListeners(
+      onActionReceivedMethod: onActionReceivedMethod,
+      onNotificationCreatedMethod: onNotificationCreatedMethod,
+      onNotificationDisplayedMethod: onNotificationDisplayedMethod,
+      onDismissActionReceivedMethod: onDismissActionReceivedMethod,
+    );
+  }
+
+  /// Use this method to detect when a new notification or a schedule is created
+  static Future<void> onNotificationCreatedMethod(
+      ReceivedNotification receivedNotification) async {
+    debugPrint('onNotificationCreatedMethod');
+  }
+
+  /// Use this method to detect every time that a new notification is displayed
+  static Future<void> onNotificationDisplayedMethod(
+      ReceivedNotification receivedNotification) async {
+    debugPrint('onNotificationDisplayedMethod');
+  }
+
+  /// Use this method to detect if the user dismissed a notification
+  static Future<void> onDismissActionReceivedMethod(
+      ReceivedAction receivedAction) async {
+    debugPrint('onDismissActionReceivedMethod');
+  }
+
+  /// Use this method to detect when the user taps on a notification or action button
+  static Future<void> onActionReceivedMethod(
+      ReceivedAction receivedAction) async {
+    debugPrint('onActionReceivedMethod');
+    final payload = receivedAction.payload ?? {};
+    if (payload["navigate"] == "true") {
+      MyApp.navigatorKey.currentState?.push(
+        MaterialPageRoute(
+          builder: (_) => const MyHomePageScreen(),
+        ),
+      );
+    }
+  }
+
+  static Future<void> showNotification({
+    required final String title,
+    required final String body,
+    final String? summary,
+    final Map<String, String>? payload,
+    final ActionType actionType = ActionType.Default,
+    final NotificationLayout notificationLayout = NotificationLayout.Default,
+    final NotificationCategory? category,
+    final String? bigPicture,
+    final List<NotificationActionButton>? actionButtons,
+    final bool scheduled = false,
+    final int? interval,
+    final int? hour,
+    final int? minute,
+  }) async {
+    await AwesomeNotifications().createNotification(
+      content: NotificationContent(
+        id: -1,
+        channelKey: 'high_importance_channel',
+        title: title,
+        body: body,
+        backgroundColor: kMainColor,
+        // customSound: "assets/audio/Bubble.mp3",
+        // icon: "assets/images/logo_wtn.png",
+        displayOnBackground: true,
+        displayOnForeground: true,
+        actionType: actionType,
+        notificationLayout: notificationLayout,
+        summary: summary,
+        category: category,
+        payload: payload,
+        bigPicture: bigPicture,
+      ),
+      actionButtons: actionButtons,
+      schedule: scheduled
+          ? NotificationCalendar(hour: hour, minute: minute, repeats: true)
+          : null,
+    );
+  }
+}
+
+
+// 1minuto = 60s 1h = 60min*60 360s
+
+/* 
+Notification Schedule based on calendar components. At least one date parameter is required. [interval] Time interval between each notification (minimum of 60 sec case repeating) [allowWhileIdle] Displays the notification, even when the device is low battery [repeats] Defines if the notification should play only once or keeps repeating [preciseAlarm] Requires maximum precision to schedule notifications at exact time, but may use more battery. Requires the explicit user consent for Android 12 and beyond. [timeZone] time zone identifier as reference of this schedule date.
+
+
+ */
